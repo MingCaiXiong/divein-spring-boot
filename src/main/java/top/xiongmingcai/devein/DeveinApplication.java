3be7e0143b0f2a3f9e9185032403595978f08046
xmc <xmc000@icloud.com>
2022-07-16 17:42:46 +0800

    spring init

---
+++ src/main/java/top/xiongmingcai/devein/DeveinApplication.java
@@ -0,0 +1,13 @@
+package top.xiongmingcai.devein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/xiongmingcai/devein/DeveinApplicationTests.java
@@ -0,0 +1,13 @@
+package top.xiongmingcai.devein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
